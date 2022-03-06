3c05c8da459ce59c31e03d1b60ce887b2c0aca17
Teutsch Mihály - Richárd <mlteutsch@gmail.com>
2022-03-06 20:24:30 +0200

    Add files via upload

---
+++ UserApp/UserApp/ViewModel/CollectionViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using UserApp.Model;
+
+namespace UserApp.ViewModel
+{
+    public class CollectionViewModel
+    {
+        //public List<User> Users { get; set; }
+        public ObservableCollection<User> Users { get; set; }
+        public CollectionViewModel()
+        {
+            Users = new ObservableCollection<User>();
+            Users.Add(new User() { Name = "Administrator" });
+            Users.Add(new User() { Name = "root" });
+            Users.Add(new User() { Name = "Blackhawk" });
+            Users.Add(new User() { Name = "User001" });
+            Users.Add(new User() { Name = "Cortana" });
+        }
+    }
+}
---
+++ UserApp/UserApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserApp.Model;
+
+namespace UserApp.ViewModel
+{
+    class MainViewModel
+    {
+        public User User { get; set; }
+        public MainViewModel()
+        {
+            User = new User() { Name = "Cortana" };
+        }
+        public void ChangeName(string newName)
+        {
+            User.Name = newName;
+        }
+    }
+}
