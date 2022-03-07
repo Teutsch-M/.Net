c703f9a8b4f2f07576a9046b64b616037a432130
Teutsch Mihály - Richárd <mlteutsch@gmail.com>
2022-03-07 09:39:14 +0200

    Add files via upload

---
+++ EmployeeApp/ViewModel/Base/RelayCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace EmployeeApp.ViewModel.Base
+{
+    public class RelayCommand : ICommand
+    {
+        private Action action;
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+        public RelayCommand(Action action) => this.action = action;
+        public bool CanExecute(object parameter) => true;
+        public void Execute(object parameter) => action();
+    }
+}
+
